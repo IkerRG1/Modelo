70bb16562287893771f44c967e4ad1164c73fb89
IkerRG1 <33682806+IkerRG1@users.noreply.github.com>
2018-01-24 09:18:49 +0100

    Add files via upload

---
+++ P04 Suma de N numeros.py
@@ -0,0 +1,7 @@
+suma = 0
+
+N = int(input('Dime un número:'))
+for i in range (N+1):
+  print(i)
+  suma = suma+i
+print(suma)
---
+++ P04. Suma 10 numeros.py
@@ -0,0 +1,6 @@
+suma = 0
+
+for i in range (1, 10+1):
+  print(i)
+  suma = suma+i
+print(suma)
